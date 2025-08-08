081aec43db7b280b1e64041bf344dd741f2a3f3e
Max-climber <max.izrailev04@mail.ru>
2025-08-09 00:46:41 +0300

    added updatePosts. every 5 sec

--- src/main.js
+++ src/main.js
@@ -41,13 +41,33 @@ i18nextInit().then(() => {
     },
   });
 
-  elements.form.addEventListener('submit', async (e) => {
+// раз в 5 секунд проверяем каждый RSS-поток, и если он содержит новые посты, добавляем их в список
+const updatePosts = () => {
+  const request = state.form.feeds.map((feed) => {
+    const proxyURL = new URL('https://allorigins.hexlet.app/get')
+      proxyURL.searchParams.append('disableCache', 'true') //отключаем кеш
+      proxyURL.searchParams.append('url', feed.url) 
+
+      return axios.get(proxyURL)
+        .then((response) => {
+          const { posts } = domParser(response.data.contents);
+          const newPosts = posts.filter((post) => !state.form.posts.some((existedPost) => existedPost.link === post.link))
+
+          state.form.posts.push(...newPosts);
+        })
+      })
+    
+      Promise.all(request)
+        .finally(() => setTimeout(updatePosts, 5000))
+ }  
+
+  elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url').trim();
 
     try {
-      await validateUrl(state.form.feeds).validate({ url });
+      validateUrl(state.form.feeds).validate({ url });
       //загружаем RSS с помощью прокси, парсим, обновляем состояние
       const proxyURL = new URL('https://allorigins.hexlet.app/get')
       proxyURL.searchParams.append('disableCache', 'true') //отключаем кеш
@@ -57,15 +77,21 @@ i18nextInit().then(() => {
         .then(response => {
           console.log('Успешный ответ:', response.data);
           const { feed, posts } = domParser(response.data.contents);
-      
-          state.form.feeds.push(feed);
+
+          state.form.feeds.push({...feed, url});
+          console.log(state.form.feeds);
           state.form.posts.push(...posts);
+
 
           state.form.error = null;
           elements.feedback.classList.remove('text-danger');
           elements.feedback.classList.add('text-success');
 
           elements.feedback.textContent = i18next.t('success');
+
+          if(state.form.feeds.length === 1) {
+            updatePosts();
+          }
         }) 
     } catch (err) {
       state.form.error = err.message;
