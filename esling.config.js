f3f0c3109fa28bde199a36fcd8e97538bb04fe59
Max-climber <max.izrailev04@mail.ru>
2025-08-13 23:14:54 +0300

    changed format of defineConfig

--- esling.config.js
+++ esling.config.js
@@ -3,10 +3,4 @@ import globals from 'globals'
 import { defineConfig } from 'eslint/config' 
 import stylistic from '@stylistic/eslint-plugin'
 
-export default defineConfig([ stylistic.configs.recommended, 
-                              { files: ['/*.{js,mjs,cjs}'], 
-                              plugins: { js },
-                              extends: ['js/recommended'] },
-                              { files: ['/*.{js,mjs,cjs}'],
-                              languageOptions: { globals: globals.browser } },
-])
+export default defineConfig([ stylistic.configs.recommended, { files: ['/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] }, { files: ['/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } }, ])
